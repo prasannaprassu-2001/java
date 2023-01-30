f0c60af80a1dd0d797f8aa2eb502f239fde41956
prasannaprassu-2001 <amuprassu11@gmail.com>
2023-01-30 16:26:37 +0530

    var

---
+++ Java-Practice/Variable.java
@@ -0,0 +1,12 @@
+class variable{
+    int a = 20;         //Instance variable
+    static int b = 40;  // Static variable
+    public static void main(String args[]){
+        int c = 60;     //Local variable
+    
+    variable v1=new variable();
+    System.out.println(v1.a);
+    System.out.println(b);
+    System.out.println(c);
+    }
+}
