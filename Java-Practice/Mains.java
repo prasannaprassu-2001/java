d941742f4ff969bb014bc31b0624786d0d2ffbc9
prasannaprassu-2001 <amuprassu11@gmail.com>
2023-02-01 12:28:13 +0530

    ma

---
+++ Java-Practice/Mains.java
@@ -0,0 +1,17 @@
+public class Mains {
+     public static void main(String[] args){
+        System.out.println("Hello!");
+    }
+
+//public - is access modifier ,we can access anywhere
+//valid access modifiers - are synchronized,final,strictfp.
+//static - without creating an object we can access
+//void - return type
+boolean m1(){
+    return true;
+}
+//main method - it is available in jvm
+//String args[]-command line arguments ,without these we can compile but we cant execute.
+
+
+}
