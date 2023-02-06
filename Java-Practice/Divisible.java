b68b1295d1f92fb6f4a6a9ff6c9f3cc3cd578537
prasannaprassu-2001 <amuprassu11@gmail.com>
2023-02-06 14:05:06 +0530

    pos

--- Java-Practice/Divisible.java
+++ Java-Practice/Divisible.java
@@ -1,10 +1,19 @@
+
 public class Divisible {
-    public static void main(String args[]){
-    int n = -127;
-    if(n>0){
-      System.out.println("True");
-    } else {
-        System.out.println("False");
+   static String check(int x){
+        if(x > 0){
+            return "true";
+          } else if(x < 0) {
+             return "false";
+          }else{
+            return "zero";
+          }
+
     }
-}
+
+    public static void main(String args[]){
+    int first = -127;
+System.out.println(first +"is"+ check(first));
+    
+    }
 }
